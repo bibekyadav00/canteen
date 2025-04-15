ff0cadd57f962edc23a4b2100ab74c5103fc4740
Bibek Yadav <bibekyadav00@gmail.com>
2025-04-15 20:01:12 +0530

    Revert "uuu"
    This reverts commit a8b7b13cb66de1c4ac39d774a07b4ebe6eea4dde.

--- components/special-offers.tsx
+++ components/special-offers.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import Image from "next/image"
 import Link from "next/link"
-// asdasd
+
 export default function SpecialOffers() {
   const offers = [
     {
